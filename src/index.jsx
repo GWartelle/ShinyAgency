6bd27c30c615d2363790691e162343add505fb26
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-02-17 20:51:09 +0100

    Premiers Setups

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/Home'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
+  document.getElementById('root'),
 )
