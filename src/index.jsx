6c373c9d41e86e5d0e040d233912c810887e75ef
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-02-18 17:01:07 +0100

    Création composant Card

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
-import Header from './components/Header'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import Header from './components/Header'
 import Error from './components/Error'
 
 ReactDOM.render(
@@ -15,8 +15,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path='/results' element={<Results />} />
-        <Route path='/freelances' element={<Freelances />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
